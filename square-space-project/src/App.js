7c9dc88f5567f61379e007003b461f5f412fa0f9
SquareSpaceKK <squarespacekk@gmail.com>
2025-03-06 19:07:22 +0800

    Add files via upload

---
+++ square-space-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Square Space</h1>
+      <p>Booking system coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
